806092877cc2d6440e32c46907caec8eb5b67c14
vrush-213 <vrushsenbansode@gmail.com>
2023-02-01 13:57:17 +0530

    first through eclipse

---
+++ dayone/src/org/tnsf/intro/SimpleJavaPorgamDemo.java
@@ -0,0 +1,11 @@
+package org.tnsf.intro;
+
+public class SimpleJavaPorgamDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		System.out.println("WWWWWWWWWWWWW");
+	}
+
+}
