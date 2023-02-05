3328a7dd65a7ad3e70ac5a234018050650a23ca0
vrushsen nanasaheb bansode <74109623+vrush-213@users.noreply.github.com>
2023-02-05 21:53:22 +0530

    Create SumOfDigits.java

---
+++ dayone/src/org/tnsf/intro/SumOfDigits.java
@@ -0,0 +1,11 @@
+public class SumOfDigits {
+  public static int sumOfDigits(int n) {
+    if (n == 0) return 0;
+    return n % 10 + sumOfDigits(n / 10);
+  }
+
+  public static void main(String[] args) {
+    int n = 12345;
+    System.out.println("The sum of digits in " + n + " is: " + sumOfDigits(n));
+  }
+}
